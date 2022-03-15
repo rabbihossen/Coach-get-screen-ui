ac3b7e022d15ed0c8500d75525847c70fda1b1e2
rabbihossen <joyahmedauthor@gmail.com>
2022-03-16 03:53:45 +0600

    First Commit

---
+++ day_1/lib/main.dart
@@ -0,0 +1,17 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+
+import 'home.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: MyHomePage(),
+    );
+  }
+}
